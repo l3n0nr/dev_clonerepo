a2bd0bb2f1af520a6f9a43719579cefc8e05081b
lenonr <lenonrmsouza@gmail.com>
2018-03-10 12:04:23 -0300

    file otimized

--- clone_repositories.sh
+++ clone_repositories.sh
@@ -1,37 +1,83 @@
 #!/bin/zsh
-#############################
-# References 
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# # # # # # # # # # # # # #
+#   	REFERENCES  	  #
+# # # # # # # # # # # # # #
 #	<http://www.yourownlinux.com/2013/10/working-with-arrays-in-bash-scripting.html>
 #
-#############################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# # # # # # # # # # # #
+#   DEVELOPMENT BY 	  #
+# # # # # # # # # # # #
 #
+# por lenonr(Lenon Ricardo)
+#       contato: <lenonrmsouza@gmail.com>
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#									      									  #
+#	If I have seen further it is by standing on the shoulders of Giants.      #
+#							~Isaac Newton	      							  #
+#									      									  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# # # # # # # # # # # # # # # # # # # # # # # ## # # #
+# Script version:           		[0.0.10.0.0.0]   #
+# Date create script:    	  		[09/03/18]       #
+# Last modification script: 		[10/03/18]       #
+# # # # # # # # # # # # # # # # # # # # # # # ## # # #
+#
+# Subtitle: a.b.c.d.e.f
+# 	a = alpha[0], beta[1], stable[2], freeze[3];
+#
+# 	b = errors in execution;
+#
+# 	c = interactions of the script;
+#
+# 	d = corrections necessaries;
+#
+# 	e = pendences;
+#
+# 	f = development;
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # HEAD
-#
 # arrays of repositories
 SERVER="http://www.github.com/lenonr/"
+LOCAL_HOME=($HOME)
+LOCAL="$LOCAL_HOME/Github/"
 REPOS=(dev_xfce dev_scripts dev_ksp dev_sysadmin dev_web)
 GIT=".git"
-LOCAL="/home/lenonr/Github/"
-
+#
 # Structure
 # Step's
 # 0 - Find directories
 # 1 - Show directories
-# 2 - Clone directories not found's
-
-#############################
+# 2 - Clone directories not found's in home user
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # BODY
 #
+### Instructions
+# Run script in you home user folder
+
 clear
 printf "############################\n"
+
+# create folder not found
+if [ -e "$LOCAL" ]; then 	  	 
+	printf ""
+else
+	mkdir $LOCAL_HOME/Github
+fi
 
 # # walk to the array
 for (( i = 0; i <= ${#REPOS[@]}; i++ )); do	
 	# verify local repo disk
 	if [[ $LOCAL${REPOS[$i]} != $LOCAL ]]; then
-		# echo $LOCAL${REPOS[$i]}
-
 		# verify local repo
 		if [ -e "$LOCAL${REPOS[$i]}" ]; then 	  	 
 		  	echo "[+] - Found:" $LOCAL${REPOS[$i]}
@@ -46,8 +92,7 @@ for (( i = 0; i <= ${#REPOS[@]}; i++ )); do
 done	
 
 printf "############################\n"
-
-#############################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # FOOTER
 #
